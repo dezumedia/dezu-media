08cb40b4958780b2e0d7963a0199e54cffb565fb
Arta Hendraa <artahenda@gmail.com>
2024-05-27 23:21:34 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,36 @@
 import MobileCarousel from "@/components/custom/mobile-carousel";
 
-const Home = () => {
+import {
+  BlocksRenderer,
+  type BlocksContent,
+} from "@strapi/blocks-react-renderer";
+import "../styles/content.css";
+
+const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+async function getDataPosts() {
+  const res = await fetch(`${apiUrl}/posts`, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+  const data = res.json();
+  return data;
+}
+
+export default async function Home() {
+  const { data } = await getDataPosts();
+
+  const content: BlocksContent = data[1].attributes.content;
   return (
     <>
       <main className="desktop-w">
         <MobileCarousel />
+        <article className="px-4">
+          <BlocksRenderer content={content} />
+        </article>
       </main>
     </>
   );
-};
-
-export default Home;
+}
--- src/components/custom/mobile-carousel.tsx
+++ src/components/custom/mobile-carousel.tsx
@@ -4,8 +4,6 @@ import {
   Carousel,
   CarouselContent,
   CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
 import Image from "next/image";
@@ -19,45 +17,32 @@ const images = [
   "technology.webp",
 ];
 
-const token = process.env.NEXT_PUBLIC_ACCESS_TOKEN;
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
-
-async function getPosts() {
-  const res = await fetch(`${apiUrl}/posts`, {
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-  });
-
-  if (!res.ok) {
-    throw new Error("fetch data failed");
-  }
-  return res.json();
-}
-
-console.log(getPosts());
-
 const MobileCarousel = () => {
   const plugin = React.useRef(
     Autoplay({ delay: 3000, stopOnInteraction: true })
   );
+
   return (
-    <Carousel plugins={[plugin.current]}>
-      <CarouselContent>
-        {images.map((image, index) => (
-          <CarouselItem className="md:basis-1/2 lg:basis-full" key={index + 1}>
-            <Image
-              src={`/${image}`}
-              alt="fish"
-              width={500}
-              height={300}
-              className="w-full h-1/3 object-cover object-top"
-            />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-    </Carousel>
+    <>
+      <Carousel plugins={[plugin.current]}>
+        <CarouselContent>
+          {images.map((image, index) => (
+            <CarouselItem
+              className="md:basis-1/2 lg:basis-full"
+              key={index + 1}
+            >
+              <Image
+                src={`/${image}`}
+                alt="fish"
+                width={500}
+                height={300}
+                className="w-full  object-cover object-top"
+              />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
+    </>
   );
 };
 
